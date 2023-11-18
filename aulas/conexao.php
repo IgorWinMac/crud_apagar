6df829b0fa9b33e89312943331f6aae2b99b2f24
IgorWinMac <38919725+IgorWinMac@users.noreply.github.com>
2023-11-17 21:25:18 -0300

    Enviados

---
+++ aulas/conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "senac";
+$database = "phpnoturno";
+$port = 3307;
+
+$connection = mysqli_connect($host,$user,$password,$database,$port);
+
+/*if($connetion){
+    echo "Conectado com sucesso!";
+}*/
+
+?>
